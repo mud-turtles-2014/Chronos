fbc465b4795b3bcf660c26b5e1815dc09618ea2b
Alex Ung & chris wallwork <pair+alexung.cwallwork@devbootcamp.com>
2014-09-26 20:36:23 -0400

    add posts within user.rb, unworking signup/login

--- app/controllers/user.rb
+++ app/controllers/user.rb
@@ -1,3 +1,5 @@
+enable "sessions"
+
 get "/" do
   erb :index
 end
@@ -23,6 +25,28 @@ get '/user' do
   erb :'users/create_user'
 end
 
+post '/user' do
+  user = User.new(params[:user])
+  if user.valid?
+    user.save
+    redirect "/session"
+  else
+    redirect "/user"
+  end
+end
+
 get '/session' do
   erb :'users/form'
+end
+
+post '/session' do
+  user = User.authenticate(params[:username], params[:password])
+
+  if user == nil || user == false
+    session[:error] = "Login Failed"
+    redirect "/session"
+  else
+    session[:user_id] = user.id
+    redirect "/user/#{user.id}"
+  end
 end
