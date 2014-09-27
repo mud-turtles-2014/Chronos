85c79beb29336c46ded00a1f1233d5097d5a6204
Alex Ung <alexung.ucsb@gmail.com>
2014-09-27 16:26:18 -0400

    Got create capsule redirecting to work

--- app/controllers/create_capsule.rb
+++ app/controllers/create_capsule.rb
@@ -1,3 +1,11 @@
 get '/user/:user_id/capsule/new' do
+  @user = current_user
   erb :create_capsule
+end
+
+post '/user/:user_id/capsule/new' do
+  @user = User.find(session[:user_id])
+  capsule = Capsule.new(params[:capsule])
+  capsule.save
+  redirect "/user/#{@user.id}"
 end
