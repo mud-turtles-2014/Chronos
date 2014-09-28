1002dec16f0b54cdaba01bdf66af7d4a0ae7d8aa
Alex Ung <alexung.ucsb@gmail.com>
2014-09-27 22:56:57 -0400

    Getting edit to work

--- app/controllers/edit.rb
+++ app/controllers/edit.rb
@@ -1,8 +1,14 @@
 get "/user/:user_id/capsule/:capsule_id/edit" do
+  @user = User.find(params[:user_id])
+  @capsule = Capsule.find(params[:capsule_id])
   erb :edit
 end
 
-# post "/capsule/:id/edit" do
-#   @capsule = Capsule.find(params[:id])
-#   erb :edit
-# end
+put "/user/:user_id/capsule/:capsule_id/edit" do
+  @user = User.find(session[:user_id])
+  # @user.capsules.create(params[:capsule])
+  @capsule = Capsule.find(params[:capsule_id])
+  Capsule.update(capsule[:user_id], params[:next_time])
+  # @user.capsules.update(params[:capsule])
+  redirect "/user/#{@user.id}"
+end
